07e53856de7269d712607a9f2d82366a8c71f066
pr0n1x <pr0n1x@yandex.ru>
2024-08-20 11:35:12 +0700

    the errors collection type moved from the rs to the werr package

--- werr/collection.go
+++ werr/collection.go
@@ -1,16 +1,16 @@
-package rs
+package werr
 
 import (
 	"errors"
 	"strings"
 )
 
-type ErrorCollection struct {
+type Collection struct {
 	sep  string
 	errs []error
 }
 
-func CollectErrors(errs []error, sep ...string) *ErrorCollection {
+func Collect(errs []error, sep ...string) *Collection {
 	if len(errs) < 1 {
 		return nil
 	}
@@ -24,49 +24,51 @@ func CollectErrors(errs []error, sep ...string) *ErrorCollection {
 	if len(errList) < 1 {
 		return nil
 	}
-	res := ErrorCollection{errs: errList, sep: "\n"}
+	res := Collection{errs: errList, sep: "\n"}
 	if len(sep) > 0 && sep[0] != "" {
 		res.sep = sep[0]
 	}
 	return &res
 }
 
-func (e *ErrorCollection) Error() (res string) {
+func (e *Collection) Error() (res string) {
 	if e == nil || len(e.errs) == 0 {
 		return "empty errors collection"
 	}
-	return strings.Join(Map(e.errs, func(i int, err error) string {
-		return err.Error()
-	}), "\n")
+	msgs := make([]string, len(e.errs))
+	for i, err := range e.errs {
+		msgs[i] = err.Error()
+	}
+	return strings.Join(msgs, "\n")
 }
 
-func (e *ErrorCollection) Empty() bool {
+func (e *Collection) Empty() bool {
 	return e == nil || len(e.errs) == 0
 }
 
-func (e *ErrorCollection) Len() int {
+func (e *Collection) Len() int {
 	if e == nil {
 		return 0
 	}
 	return len(e.errs)
 }
 
-func (e *ErrorCollection) Append(err error) *ErrorCollection {
+func (e *Collection) Append(err error) *Collection {
 	if err == nil {
 		return e
 	}
 	if e.Empty() {
-		return &ErrorCollection{errs: []error{err}}
+		return &Collection{errs: []error{err}}
 	}
 	errSlice := append(e.errs, err)
 	if cap(errSlice) == cap(e.errs) {
 		e.errs = errSlice
 		return e
 	}
-	return &ErrorCollection{errs: errSlice}
+	return &Collection{errs: errSlice}
 }
 
-func (e *ErrorCollection) Map(convert func(error) error) *ErrorCollection {
+func (e *Collection) Map(convert func(error) error) *Collection {
 	if e == nil || len(e.errs) < 1 {
 		return nil
 	}
@@ -80,15 +82,15 @@ func (e *ErrorCollection) Map(convert func(error) error) *ErrorCollection {
 	if len(errs) < 1 {
 		return nil
 	}
-	return &ErrorCollection{errs: errs}
+	return &Collection{errs: errs}
 }
 
-func (e *ErrorCollection) List() []error {
+func (e *Collection) List() []error {
 	// Map allocates new slice
 	return e.Map(func(err error) error { return err }).errs
 }
 
-func (e *ErrorCollection) Is(target error) bool {
+func (e *Collection) Is(target error) bool {
 	if e == target {
 		return true
 	}
